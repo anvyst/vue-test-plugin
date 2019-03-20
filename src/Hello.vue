a33b762e2f1fdb7bbfbbbec64c870132328b61ac
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-03-20 08:05:49 +0200

    Adding eslint support

---
+++ src/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    Hello, world!
+    <h3 class="hi">{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello, again!'
+    }
+  }
+}
+</script>
+
+<style>
+  .hi {
+    color: red;
+  }
+</style>
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Hello from './Hello.vue'
+
+function plugin (Vue) {
+  Vue.component('hello', Hello)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+export {
+  Hello
+}
