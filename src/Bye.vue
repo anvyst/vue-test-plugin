063d35e549a19bde7c99a71610d18a69ae479409
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-03-22 16:01:32 +0200

    Multiple components logging for one component namespace

---
+++ src/Bye.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+<script>
+export default {
+  data () {
+    name: 'Bye'
+    return {
+      message: 'See ya!'
+    }
+  }
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import Hello from './Hello.vue'
+import Bye from './Bye.vue'
 
-export default Hello
+const TestPlugin = {
+  install,
+  Hello,
+  Bye
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(install)
+}
+
+export default TestPlugin
+
+function install (Vue) {
+  if (install.installed) {
+    return
+  }
+
+  Vue.component(Hello.name, Hello)
+  Vue.component(Bye.name, Bye)
+}
